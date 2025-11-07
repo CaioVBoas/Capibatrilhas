04b9d9eab497bbaf25ed2380976b0cf2c86c7f28
danielgacioly <dgla@cin.ufpe.br>
2025-11-07 14:40:15 -0300

    style(card): add hover translate and shadow effect

--- client/src/components/featuredBoroughCards/index.tsx
+++ client/src/components/featuredBoroughCards/index.tsx
@@ -18,7 +18,7 @@ const FeaturedBoroughCards: React.FC<FeaturedBoroughCardsProps> = ({ boroughs })
     <div className="grid grid-cols-1 md:grid-cols-2 gap-6"> {/* Key por enquanto é Index, 
         mas depois substituir por ID. Tirar também o index do .map */}
       {boroughs.map((borough, index) => ( 
-        <div key={index} className="bg-white rounded-2xl shadow-sm border border-gray-200 p-5 flex flex-col hover:shadow-md transition">
+        <div key={index} className="bg-white rounded-2xl shadow-sm border border-gray-200 p-5 flex flex-col transition-all duration-300 hover:shadow-lg hover:-translate-y-1">
           
           <div className="flex justify-between items-start mb-1.5">
             <h2 className="text-xl font-semibold text-gray-900">
@@ -27,7 +27,7 @@ const FeaturedBoroughCards: React.FC<FeaturedBoroughCardsProps> = ({ boroughs })
             
 
             <span className="px-3 py-1 bg-white border border-gray-300 rounded-full text-sm font-semibold text-gray-800 shadow-sm flex items-center mr-1">
-                <Flame className="h-6 w-6 text-orange-500 fill-orange-500 mr-1" /> {borough.bonusScore} pontos
+                <Flame className="h-6 w-6 text-orange-500 fill-orange-500 mr-1" /> {borough.bonusScore} bônus
             </span>
          </div>
 
