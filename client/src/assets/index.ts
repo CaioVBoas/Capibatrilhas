c4ed4254531a43148a342ee60557f101fd5b4658
Guilherme Valença Saunders <guilhermevalenca.s7@gmail.com>
2025-11-10 18:41:11 -0300

    Imagens adicionadas para o header

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,3 +1,9 @@
 import Logo from './Logo.png';
+import Capiba from './Capiba.png';
+import Prefeitura from './Prefeitura.png';
+import  LogoCapibatrilhas from './LogoCapibatrilhas.png';
 
 export { Logo };
+export { Capiba };
+export {Prefeitura};
+export {LogoCapibatrilhas};
