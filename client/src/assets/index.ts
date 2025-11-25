8904e58040eeb443e51888dcbd0501796b3bb789
Guilherme Valença Saunders <guilhermevalenca.s7@gmail.com>
2025-11-25 17:32:02 -0300

    feat("footer"):Add footer component with Prefeitura logo
    - Created a new Footer component that displays the Prefeitura logo.
    - Imported the logo from assets and used Next.js Image component for rendering.
    - Updated assets index to include the new logo.

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,10 +1,12 @@
 import Logo from './Logo.png';
 import CalendarImg from "./calendarImg.svg";
 import logoCapibatrilhas from "./logoCapibatrilhas.svg";
+import logoPrefeitura from "./LogoDaPrefeitura.svg"
 
 
 export { CalendarImg };
 export { Logo };
 export {logoCapibatrilhas};
+export {logoPrefeitura};
 
 
